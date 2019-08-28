fac81225f5e2daac46ef0878396be3eae29c165a
hquang1999 <54284678+hquang1999@users.noreply.github.com>
2019-08-28 12:55:57 -0600

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main ()
+{
+	std::cout<< "# Hello, I'm Hieu" <<std::endl << std::endl;
+	std::cout<< "**Facts About Me:**" << std::endl;
+	std::cout<< "+ I'm an international student from Vietnam." << std::endl;
+	std::cout<< "+ I'm a third year engineering student, *2nd year of programming*." << std::endl;
+	std::cout<< "+ I enjoy watching football and socializing with friends." << std::endl;
+	std::cout<< "+ I play video games nearly on all my free time." << std::endl;
+
+
+	return 0;
+}
+
---
+++ hello.md
@@ -0,0 +1,7 @@
+# Hello, I'm Hieu
+
+**Facts About Me:**
++ I'm an international student from Vietnam.
++ I'm a third year engineering student, *2nd year of programming*.
++ I enjoy watching football and socializing with friends.
++ I play video games nearly on all my free time.
