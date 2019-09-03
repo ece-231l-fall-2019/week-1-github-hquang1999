601a662750e14047d84a0592e61b735cb2251c36
hquang1999 <hquang@unm.edu>
2019-09-03 14:09:47 -0600

    update

--- hello.cpp
+++ hello.cpp
@@ -2,17 +2,19 @@
 int main ()
 {
 	std::cout<< "# Hello, I'm Hieu" <<std::endl << std::endl;
-	std::cout<< "**###Where I'm From**" << std::endl;
+	std::cout<< "**Where I'm From:" << std::endl;
 	std::cout<< "I'm an international student from Vietnam and have been studying in the US for about 6 years." << std::endl <<std::endl;
-	std::cout<< "**##Academics**" << std::endl;
+	std::cout<< "**Academics**" << std::endl;
 	std::cout<< "+ I'm a third year engineering student, *2nd year of programming*." << std::endl;
 	std::cout<< "+ Only have the basic understanding of C while trying to grasp C++ concepts" <<std::endl << std::endl;
-	std::cout<< "**##Hobbies**" <<std::endl;
+	std::cout<< "**Hobbies**" <<std::endl;
 	std::cout<< "+ I enjoy watching football while socializing with friends and family (New England Patriots)." << std::endl;
 	std::cout<< "+ I play video games nearly on all my free time." << std::endl;
 	std::cout<< "+ I have a soft spot for PC hardware tinkering. I'm usually the dedicated IT for my friends and family. I've built many PC's and own and broke a couple due to curiosity reasons" << std::endl << std::endl;
-	std::cout<< "**##Goals For Class**"<< std::endl;
+	std::cout<< "**Goals For Class:**"<< std::endl;
 	std::cout<< "To hopefully get a bteer understanding of C++ and be able to code independently without too much help." << std::endl;
+	std::cout<< "**PS:**"<< std::endl;
+	std::cout<< "I'm not the biggest fan of animals so I dont have pets" << std::endl;
 
 	return 0;
 }
